173c090ccf40fe7e7ee2b71631af1ed86ad8b139
hansa <byreddyhansa@gmail.com>
2025-09-22 07:16:59 +0530

    Operators & Typecasting

---
+++ Tns_java_project/src/tns_package/Operators.java
@@ -0,0 +1,65 @@
+package tns_package;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		
+		
+		        int a = 10, b = 3;
+		        boolean x = true, y = false;
+
+		        //arithmetic
+		        System.out.println(a + b);
+		        System.out.println(a - b);
+		        System.out.println(a * b);
+		        System.out.println(a / b);
+		        System.out.println(a % b);
+
+		        //relational
+		        System.out.println(a == b);
+		        System.out.println(a != b);
+		        System.out.println(a > b);
+		        System.out.println(a < b);
+		        System.out.println(a >= b);
+		        System.out.println(a <= b);
+
+		        //logical
+		        System.out.println(x && y);
+		        System.out.println(x || y);
+		        System.out.println(!x);
+
+		        //bitwise
+		        System.out.println(a & b);
+		        System.out.println(a | b);
+		        System.out.println(a ^ b);
+		        System.out.println(~a);
+		        System.out.println(a << 1);
+		        System.out.println(a >> 1);
+		        System.out.println(a >>> 1);
+
+		        //assignment
+		        a += 2;
+		        System.out.println(a);
+		        a -= 1;
+		        System.out.println(a);
+		        a *= 3;
+		        System.out.println(a);
+		        a /= 2;
+		        System.out.println(a);
+		        a %= 4;
+		        System.out.println(a);
+
+		        //unary
+		        int c = 5;
+		        System.out.println(+c);
+		        System.out.println(-c);
+		        System.out.println(++c);
+		        System.out.println(--c);
+		        System.out.println(c++);
+		        System.out.println(c--);
+
+		        //boolean
+	}
+}
+ 
+
---
+++ Tns_java_project/src/tns_package/Typecast.java
@@ -0,0 +1,26 @@
+package tns_package;
+
+public class Typecast {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=55;
+		float y=x;
+		
+		System.out.println(y);  //55.0
+		
+		
+		double a = 30.97;
+		int b= (int)a;
+		
+		System.out.println(b);//30
+		
+		
+		float f = 123.45f;
+		long l = (long) f; 
+		
+		System.out.println(l); //123
+
+	}
+
+}
